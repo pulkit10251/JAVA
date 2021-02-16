90071c2a0fff3b15cc60ee4e7b0891d418510306
pulkit10251 <pulkit10251.sh@gmail.com>
2021-02-16 23:57:41 +0530

    Ques1 completed

--- JAVA-450/src/ARRAY/Q1.java
+++ JAVA-450/src/ARRAY/Q1.java
@@ -6,5 +6,20 @@ public class Q1 {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
+        System.out.println("QUES 1 : REVERSE a ARRAY !");
+
+        int[] array = new int[5];
+
+        for (int i = 0; i < array.length; i++) {
+            // Filling values
+            array[i] = s.nextInt();
+        }
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
+
+
     }
 }
