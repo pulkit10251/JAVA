51e73d42235b1d9648b5f41ab94b76156963f8ce
pulkit10251 <pulkit10251.sh@gmail.com>
2021-02-19 01:27:08 +0530

    Ques 7 Done

---
+++ JAVA-450/src/ARRAY/Q7.java
@@ -0,0 +1,22 @@
+package ARRAY;
+
+public class Q7 {
+
+    public static void main(String[] args) {
+        // Ques 7 : Write a program to cycilly rotate the array by 1;
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int last = arr[arr.length-1];
+
+        for (int i = arr.length-1; i >0; i--) {
+            arr[i]=arr[i-1];
+        }
+
+        arr[0]=last;
+
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
