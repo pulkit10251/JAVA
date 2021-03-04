76dffabe1be5ca267d6e77123a06fa48d3937f5b
pulkit10251 <pulkit10251.sh@gmail.com>
2021-03-04 18:43:56 +0530

    Q32 done

---
+++ JAVA-450/src/ARRAY/Q32.java
@@ -0,0 +1,20 @@
+package ARRAY;
+
+import java.util.Arrays;
+
+public class Q32 {
+
+    public static void main(String[] args) {
+        // Ques 32 Three Way Partitioning;
+        int[] A = {1, 2, 13, 3, 3, 4, 10, 12, 14, 16};
+        int a = 10;
+        int b = 14;
+        threeWayPartition(A, a, b);
+    }
+
+    public static void threeWayPartition(int[] A, int a, int b) {
+        Arrays.sort(A);
+        System.out.println(1);
+    }
+
+}
