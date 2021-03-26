d6dcdcd1c002445df9af904d7c91f42952c836ae
pulkit10251 <pulkit10251.sh@gmail.com>
2021-03-27 00:58:16 +0530

    Q11

---
+++ JAVA-450/src/STRING/Q11.java
@@ -0,0 +1,33 @@
+package STRING;
+
+public class Q11 {
+
+    public static void main(String[] args) {
+        String str = "ABC";
+        Permutations(str,0, 2);
+    }
+
+    public static void Permutations(String str, int l, int r) {
+
+        if (l == r) {
+            System.out.println(str);
+        }
+
+        for (int i = l; i <= r; i++) {
+            str = swap(str, l, i);
+            Permutations(str, l + 1, r);
+            str = swap(str, l, i);
+
+        }
+    }
+
+    public static String swap(String a, int i, int j) {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+
+}
