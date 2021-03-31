364828f41c158ebfaffc23f7ebd11ae738ec045b
pulkit10251 <pulkit10251.sh@gmail.com>
2021-03-31 13:19:33 +0530

    Q 15 done

---
+++ JAVA-450/src/STRING/Q15.java
@@ -0,0 +1,34 @@
+package STRING;
+
+import java.util.Stack;
+
+public class Q15 {
+
+    public static void main(String[] args) {
+        System.out.println(BalancedParenthesis("{{[()]}}}"));
+    }
+
+    public static boolean BalancedParenthesis(String str){
+        Stack<Character> stack = new Stack<>();
+        if(str.charAt(0) == '}' || str.charAt(0) == ']' || str.charAt(0)==')'){
+            return false;
+        }
+        for (int i = 0 ; i< str.length(); i++){
+            if(str.charAt(i) == '{' || str.charAt(i) == '[' || str.charAt(i)=='('){
+                stack.push(str.charAt(i));
+            }else
+
+            if(str.charAt(i) == '}' && !stack.isEmpty() &&  stack.peek() =='{'){
+                stack.pop();
+            }else if(str.charAt(i) == ')' && !stack.isEmpty() &&  stack.peek() =='('){
+                stack.pop();
+            }else if(str.charAt(i) == ']' && !stack.isEmpty() && stack.peek() =='['){
+                stack.pop();
+            }else{
+                stack.push(str.charAt(i));
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
