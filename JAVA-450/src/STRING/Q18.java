751589d32b79125aa3095adb43f05ff5d94eac33
pulkit10251 <pulkit10251.sh@gmail.com>
2021-04-04 00:20:24 +0530

    Q18 done

---
+++ JAVA-450/src/STRING/Q18.java
@@ -0,0 +1,44 @@
+package STRING;
+
+public class Q18 {
+
+    public static void main(String[] args) {
+        KMP("aaaa");
+    }
+
+    public static void KMP(String s){
+        int M = s.length();
+
+        int[] lps = new int[M];
+        lps[0] = 0;
+
+
+        int len = 0;
+
+        int i = 1;
+        while (i < M){
+            if(s.charAt(i) == s.charAt(len)){
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else{
+                if(len!=0 ){
+                    len = lps[len-1];
+                }else{
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        for (int val : lps){
+            System.out.print(val+"  ");
+        }
+        System.out.println();
+
+        int res = lps[M -1];
+        System.out.println(Math.min(res, M /2));
+
+    }
+}
