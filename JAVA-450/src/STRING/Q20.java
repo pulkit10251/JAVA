0a0e744a4bd7756108f0953f8925d5abe8865f6c
pulkit10251 <pulkit10251.sh@gmail.com>
2021-04-04 00:20:48 +0530

    Q20 done

---
+++ JAVA-450/src/STRING/Q20.java
@@ -0,0 +1,49 @@
+package STRING;
+
+import java.util.Stack;
+
+public class Q20 {
+
+    public static void main(String[] args) {
+        System.out.println(countReversals("}{{}}{{{"));
+    }
+
+    public static int countReversals(String expr){
+        int len = expr.length();
+
+        if (len%2 != 0)
+            return -1;
+
+        Stack<Character> s=new Stack<>();
+
+        for (int i=0; i<len; i++)
+        {
+            char c = expr.charAt(i);
+            if (c =='}' && !s.empty())
+            {
+                if (s.peek()=='{')
+                    s.pop();
+                else
+                    s.push(c);
+            }
+            else
+                s.push(c);
+        }
+
+        int red_len = s.size();
+        double open = 0.0;
+        double close = 0.0;
+        while (!s.empty() )
+        {
+
+            if(s.peek() == '{'){
+                open++;
+            }else{
+                close++;
+            }
+            s.pop();
+        }
+
+        return (int)(Math.ceil(open / 2) + Math.ceil(close/2));
+    }
+}
