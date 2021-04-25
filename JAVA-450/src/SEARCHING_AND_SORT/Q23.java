116c7356651fa89532174270e710ba7ffe430f8e
pulkit10251 <pulkit10251.sh@gmail.com>
2021-04-25 16:42:49 +0530

    Q23 done

---
+++ JAVA-450/src/SEARCHING_AND_SORT/Q23.java
@@ -0,0 +1,16 @@
+package SEARCHING_AND_SORT;
+
+public class Q23 {
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 4, 5, 8};
+        int[] arr2 = {2, 3, 6, 9};
+        int k = 6;
+
+        System.out.println(kthSortedArray(arr1, arr2, 0, arr1.length, 0, arr2.length, k));
+    }
+
+    public static int kthSortedArray(int[] arr1, int[] arr2, int lb1, int ub1, int lb2, int ub2, int k) {
+            return 0;
+    }
+}
