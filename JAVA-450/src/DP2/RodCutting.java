62ba89e862d4f030e0fcd8de4a59eb4b601a183b
Mudita Sharma <pulkit10251.sh@gmail.com>
2021-09-10 14:31:26 +0530

    new quest

---
+++ JAVA-450/src/DP2/RodCutting.java
@@ -0,0 +1,27 @@
+package DP2;
+
+import java.util.Scanner;
+
+public class RodCutting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] cost = new int[n];
+        for (int i = 0; i < n; i++) {
+            cost[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        for(int i = 1; i < dp.length;i++){
+            int max = 0;
+            for (int j = 1; j <= i ; j++) {
+                max = Math.max(max, dp[i-j] + cost[j-1]);
+            }
+            dp[i] = max;
+            System.out.print(dp[i]+" ");
+        }
+
+        System.out.println(dp[n]);
+    }
+}
